cfe57f0ed76d10922b16792274202ffe09944b6b
juliusverrel <juliusverrel@users.noreply.github.com>
2020-09-25 13:04:57 +0200

    use installed python 3.6

--- run.bat
+++ run.bat
@@ -1,5 +1,6 @@
 cd urge_monitor
 
-"C:\Program Files\PsychoPy3\python.exe" RunExperiment.py
+"C:\Program Files\PsychoPy3\Python.exe" RunExperiment.py
+REM "C:\Software\python36\python.exe" RunExperiment.py
 
 cd ..
