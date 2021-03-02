c0aeb0663902f0c099b0eb76fa3308988a557901
juliusverrel <juliusverrel@users.noreply.github.com>
2021-03-02 22:07:55 +0100

    add dummy experiment (just sound & LSL at start and end)

---
+++ urge_monitor/RunDummy.py
@@ -0,0 +1,76 @@
+from psychopy import core, visual, event, logging
+import sound
+from devices import LSL
+
+# some settings
+quit_key = 'q'
+start_trigger = 'start_recording'
+end_trigger = 'end_recording'
+sound_cfg = dict(value=880, duration=0.2, volume=1.0)
+message_txt = "Mit Tastendruck geht es los."
+pre_dur = 3
+exp_dur = 300
+
+
+class QuitException(Exception):
+    pass
+
+
+class DummyExp:
+    def __init__(self):
+        LSL.init('UrgeMonitor')
+        self.win = visual.Window([1920, 1080], monitor="dummy", fullscr=True, color='black')
+        self.win.mouseVisible = False
+        self.message = visual.TextStim(self.win, text=message_txt)
+        self.sound_cue = sound.AudioPeep(sound_cfg)
+        self.state = 'none'
+
+    def start_cue(self):
+        logging.info('start cue')
+        LSL.send_marker(start_trigger)
+        self.sound_cue.play()
+        
+    def end_cue(self):
+        logging.info('end cue')
+        LSL.send_marker(end_trigger)
+        self.sound_cue.play()
+
+    def show_msg_wait(self):
+        logging.info('show message')
+        self.message.draw()
+        self.win.flip()
+        event.waitKeys()
+
+    def wait_period(self, state):
+        logging.info('waiting: ' + state)
+        self.win.flip()
+        self.state = state
+        dur = pre_dur if state == 'pre' else exp_dur
+        if event.waitKeys(maxWait=dur, keyList=[quit_key]):
+            raise QuitException()
+
+    def finish(self):
+        logging.info('finish up')
+        if self.state == 'exp':
+            self.end_cue()
+            core.wait(2*sound_cfg['duration'])
+        #del self.message
+        self.win.close()
+        core.quit()
+        
+    def run(self):
+        try:
+            self.show_msg_wait()
+            self.wait_period('pre')
+            self.start_cue()
+            self.wait_period('exp')
+        except QuitException:
+            logging.info('quit exception')
+        except Exception as e:
+            logging.error('some other exception')
+            print(e)    
+        finally:
+            self.finish()
+
+logging.console.setLevel(logging.INFO)
+DummyExp().run()
