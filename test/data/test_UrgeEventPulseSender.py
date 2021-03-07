d85d899897efeeed0d7804c159fce64f60ed913c
LueBecko <waldschrat@gmx.net>
2021-03-07 21:58:42 +0100

    make value transformation a separate class

--- test/data/test_UrgeEventPulseSender.py
+++ test/data/test_UrgeEventPulseSender.py
@@ -12,17 +12,45 @@ class UrgeEventPulseSenderConstructorTest(unittest.TestCase):
         self.assertIsInstance(UrgeEventPulseSender(PulseOutputNone()), UrgeEventPulseSender)
 
 class UrgeEventPulseSenderTransformationTest(unittest.TestCase):
-    sut = UrgeEventPulseSender(PulseOutputNone())
+    pulseOutputStub = PulseOutputNone()
+    sut = UrgeEventPulseSender(pulseOutputStub)
 
-    def test_ransformUrgeValueIntoPulseWithMinimumUrge_returns1(self):
-        self.assertEqual(1 ,self.sut.transformUrgeValueIntoPulse(0));
+    def test_onEventSendsTransformedValue(self):
+        self.sut.onEvent(0, 1.5, 0.01, [])
+        self.assertEqual(1, self.pulseOutputStub.__data__);
 
-    def test_ransformUrgeValueIntoPulseWithMaximumUrge_returns255(self):
-        self.assertEqual(255 ,self.sut.transformUrgeValueIntoPulse(1));
+        self.sut.onEvent(0.5, 1.5, 0.01, [])
+        self.assertEqual(128, self.pulseOutputStub.__data__);
 
-    def test_ransformUrgeValueIntoPulseWithMedianUrge_returns128(self):
-        self.assertEqual(128 ,self.sut.transformUrgeValueIntoPulse(0.5));
+        self.sut.onEvent(1, 1.5, 0.01, [])
+        self.assertEqual(255, self.pulseOutputStub.__data__);
 
+class UrgeEventPulseTransformatorTest(unittest.TestCase):
+    def test_constructorValidParameters_passes(self):
+        UrgeEventPulseTransformator(1, 255);
+
+    def test_constructorInvalidParameters_raises(self):
+        with self.assertRaises(AssertionError): UrgeEventPulseTransformator(0.5, 100)
+        with self.assertRaises(AssertionError): UrgeEventPulseTransformator('low', 100)
+        with self.assertRaises(AssertionError): UrgeEventPulseTransformator(1, 100.5)
+        with self.assertRaises(AssertionError): UrgeEventPulseTransformator(1, 'high')
+
+        with self.assertRaises(AssertionError): UrgeEventPulseTransformator(0, 100)
+        with self.assertRaises(AssertionError): UrgeEventPulseTransformator(1, 256)
+
+        with self.assertRaises(AssertionError): UrgeEventPulseTransformator(256, 1)
+
+    def test_transformMinimumUrgeValue_returnsLow(self):
+        sut = UrgeEventPulseTransformator(100, 199);
+        self.assertEqual(100 ,sut.transform(0));
+
+    def test_transformMaximumUrgeValue_returnsHigh(self):
+        sut = UrgeEventPulseTransformator(100, 199);
+        self.assertEqual(199 ,sut.transform(1));
+
+    def test_transformMediumUrgeValue_returnsMiddle(self):
+        sut = UrgeEventPulseTransformator(100, 199);
+        self.assertEqual(149 ,sut.transform(0.5));
 
 if __name__ == '__main__':
     unittest.main()
--- urge_monitor/data/UrgeEventPulseSender.py
+++ urge_monitor/data/UrgeEventPulseSender.py
@@ -7,18 +7,33 @@ def applyFiringPattern(pulseOutputDevice, pulseConfiguration, dataHandler):
         pulseConfiguration['pulse']['firing_pattern'] == PulseOutput.PulseFiringPattern.ON_URGE_RECORD):
         dataHandler.registerUrgeRecordListener(UrgeEventPulseSender(pulseOutputDevice))
 
+class UrgeEventPulseTransformator:
+    ''' descirbes how to transform the internal urge value to a parallel out pulse value '''
+    def __init__(self, low, high):
+        # assert provided values
+        assert isinstance(low, int)
+        assert isinstance(high, int)
+        assert low > 0
+        assert high > low
+        assert high < 256
+
+        self.__low__ = low;
+        self.__high__ = high;
+        self.__range__ = high - low;
+
+    def transform(self, urgevalue):
+        return int(urgevalue * self.__range__) + self.__low__;    
+
 class UrgeEventPulseSender(UrgeEventListener.UrgeEventListener):
     ''' sends a pulse to the specified output device when a urge event occures '''
     def __init__(self, pulseOutput):
         assert isinstance(pulseOutput, PulseOutput.PulseOutput)
         self.__pulseOutput = pulseOutput
+        self.__transformator__ = UrgeEventPulseTransformator(1, 255)
 
     def onEvent(self, urgeValue, recTime, lag, buttons):
-        self.__pulseOutput.setDataValue(self.transformUrgeValueIntoPulse(urgeValue))
+        self.__pulseOutput.setDataValue(self.__transformator__.transform(urgeValue))
         self.__pulseOutput.sendPulse()
-
-    def transformUrgeValueIntoPulse(self, urgeValue):
-        return int(urgeValue * 254.0) + 1
 
     def close(self):
         pass
